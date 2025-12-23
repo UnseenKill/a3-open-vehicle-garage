59fec289176ef4fd4f6a51f38a0ed611ab319c6d
Oliver Schieche <post@oliver-schieche.de>
2025-12-23 10:54:34 +0100

    Moved A3OVG_ macros to "script_macros.hpp" and more CBA related ones to "script_macros_common.hpp"

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,8 +1,24 @@
 #include "script_macros_common.hpp"
 
-#ifndef QPREFIX
-    #define QPREFIX QUOTE(PREFIX)
-#endif
-#ifndef QQPREFIX
-    #define QQPREFIX QUOTE(QUOTE(PREFIX))
-#endif
+#define A3OVG_CONFIG_CLASS_BASE TRIPLES(PREFIX,Config,Base)
+#define A3OVG_CONFIG_CLASS DOUBLES(PREFIX,Config)
+#define A3OVG_FEATURE_ENABLED(config,feature) (getNumber(_config >> "Features" >> QUOTE(feature)) != 0)
+#define A3OVG_FUNCTION_PREAMBLE(function) \
+    TRACE_1(function,_this); \
+    if isNull(localNamespace getVariable[QEGVAR(core,configVerified), configNull]) exitWith { \
+        ERROR_3(QUOTE(ARR_2(Configuration not found. Abort call to QQUOTE(%1) from %2,line %3.)),function,__FILE__,__LINE__); \
+        if (hasInterface) then { \
+            [LELSTRING(API,ConfigVerificationFailed)] call BIS_fnc_error; \
+        }; \
+    };
+
+#define A3OVG_GET_CONFIG(var) \
+    private var = localNamespace getVariable[QEGVAR(core,configVerified), configNull]; \
+    if !assert(!isNull(var)) exitWith {}
+
+#define A3OVG_UI_PUSH_CONTEXT() \
+    (EGVAR(ui,context) pushBack [])
+#define A3OVG_UI_POP_CONTEXT() \
+    (EGVAR(ui,context) deleteAt(count EGVAR(ui,context) - 1))
+
+#define A3OVG_VEH_NAME(vehicle) getText((if (vehicle isEqualType objNull) then[{configOf(vehicle)}, {configFile >> QUOTE(CfgVehicles) >> (vehicle)}]) >> QUOTE(displayName))
--- addons/main/script_macros_common.hpp
+++ addons/main/script_macros_common.hpp
@@ -1,27 +1,6 @@
 #define MAINPREFIX z
 #include "\x\cba\addons\main\script_macros_common.hpp"
 
-#define A3OVG_CONFIG_CLASS_BASE TRIPLES(PREFIX,Config,Base)
-#define A3OVG_CONFIG_CLASS DOUBLES(PREFIX,Config)
-#define A3OVG_FEATURE_ENABLED(config,feature) (getNumber(_config >> "Features" >> QUOTE(feature)) != 0)
-#define A3OVG_FUNCTION_PREAMBLE(function) \
-    TRACE_1(function,_this); \
-    if isNull(localNamespace getVariable[QEGVAR(core,configVerified), configNull]) exitWith { \
-        ERROR_3(QUOTE(ARR_2(Configuration not found. Abort call to QQUOTE(%1) from %2,line %3.)),function,__FILE__,__LINE__); \
-        if (hasInterface) then { \
-            [localize ELSTRING(API,ConfigVerificationFailed)] call BIS_fnc_error; \
-        }; \
-    };
-
-#define A3OVG_GET_CONFIG(var) \
-    private var = localNamespace getVariable[QEGVAR(core,configVerified), configNull]; \
-    if !assert(!isNull(var)) exitWith {}
-
-#define A3OVG_UI_PUSH_CONTEXT() \
-    (EGVAR(ui,context) pushBack [])
-#define A3OVG_UI_POP_CONTEXT() \
-    (EGVAR(ui,context) deleteAt(count EGVAR(ui,context) - 1))
-
 // HEMTT complains about padded arguments; the original macro has a newline in it
 #undef PFORMAT_2
 #undef PFORMAT_3
@@ -56,3 +35,22 @@
     CFGFUNCTION_HEADER;\
     RECOMPILE;\
 }
+
+#ifndef QPREFIX
+    #define QPREFIX QUOTE(PREFIX)
+#endif
+#ifndef QQPREFIX
+    #define QQPREFIX QUOTE(QUOTE(PREFIX))
+#endif
+
+#ifndef QQUOTE
+    #define QQUOTE(x) QUOTE(QUOTE(x))
+#endif
+
+#ifndef QELSTRING
+    #define QELSTRING(section,string) QUOTE(ELSTRING(section,string))
+#endif
+
+#ifndef LQELSTRING
+    #define LQELSTRING(section,string) localize QELSTRING(section,string)
+#endif
