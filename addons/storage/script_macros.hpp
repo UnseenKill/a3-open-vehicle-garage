289e2a73ae86ed0a9ef9185388bca7b0ebc1d00f
Oliver Schieche <post@oliver-schieche.de>
2025-12-28 13:34:14 +0100

    Prevent A3 macro argument inheritance

--- addons/storage/script_macros.hpp
+++ addons/storage/script_macros.hpp
@@ -1,16 +1,18 @@
-#define METHOD_ABSTRACT(method) \
-    [#method, { throw format["abstract method %1::%2() called on base class", QADDON, #method] }]
-#define METHOD_DEFINE_NAME(method,function) [#method, { METHOD_INVOKE(function) }]
-#define METHOD_DEFINE(method) METHOD_DEFINE_NAME(method,method)
+#define SECTION_VEHICLE QUOTE(vehicle)
+
+#define METHOD_ABSTRACT(methodName) \
+    [#methodName, { throw format["abstract method %1::%2() called on base class", QADDON, #methodName] }]
+#define METHOD_DEFINE_NAME(methodName,functionName) [#methodName, { METHOD_INVOKE(functionName) }]
+#define METHOD_DEFINE(methodName) METHOD_DEFINE_NAME(methodName,methodName)
 #define METHOD_INVOKE(name) [_self, _this] call FUNC(DOUBLES(method,name))
 
-#define METHOD_PREAMBLE_RET(method,return) \
-    TRACE_1(format[ARR_3("%1::%2",((_this#0) get "#type" select 0),QUOTE(method))],_this#1); \
+#define METHOD_PREAMBLE_RET(methodName,return) \
+    TRACE_1(format[ARR_3("%1::%2",((_this#0) get "#type" select 0),QUOTE(methodName))],_this#1); \
     if !assert(params[ \
         ["_self", nil], \
         ["_"+"this", nil, []] \
     ]) exitWith { return }
-#define METHOD_PREAMBLE(method) METHOD_PREAMBLE_RET(method,nil)
+#define METHOD_PREAMBLE(methodName) METHOD_PREAMBLE_RET(methodName,nil)
 
-#define SUPER(method) ([_self, #method] call EFUNC(storage,getParentMethod))
+#define SUPER(methodName) ([_self, #methodName] call EFUNC(storage,getParentMethod))
 #define THIS_CLASS (_self get "#type" select 0)
