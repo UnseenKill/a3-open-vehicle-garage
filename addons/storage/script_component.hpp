c63460347c8fec1c657361fd5589e3d2ebc7b6a4
Oliver Schieche <post@oliver-schieche.de>
2025-12-30 20:42:35 +0100

    Too much RPT spam

--- addons/storage/script_component.hpp
+++ addons/storage/script_component.hpp
@@ -1,4 +1,5 @@
 #define COMPONENT storage
+#define __A3OVG_PRODUCTION__
 #include "\z\a3ovg\addons\main\script_mod.hpp"
 #include "\z\a3ovg\addons\main\script_macros.hpp"
 #include "script_macros.hpp"
--- addons/storage_inidbi2/script_component.hpp
+++ addons/storage_inidbi2/script_component.hpp
@@ -1,3 +1,4 @@
 #define COMPONENT storage_inidbi2
+#define __A3OVG_PRODUCTION__
 #include "\z\a3ovg\addons\main\script_mod.hpp"
 #include "\z\a3ovg\addons\main\script_macros.hpp"
