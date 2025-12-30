9c6e88559fac2b85267de8d41f43813a5d126877
Oliver Schieche <post@oliver-schieche.de>
2025-12-30 08:14:36 +0100

    Moved close button, suffixed stringtable entries, disable all main controls on load

--- addons/ui/dialog.hpp
+++ addons/ui/dialog.hpp
@@ -13,12 +13,14 @@ FORWARD(RscText);
 FORWARD(RscTree);
 #undef FORWARD
 
-class GVAR(RscTitleBar) : RscText {
-    y = QUOTE(safeZoneY + 0.1 * safeZoneH);
-    h = QUOTE(safeZoneH * 0.03);
-    w = QUOTE(safeZoneW * 0.4);
+class GVAR(RscButton): RscButton {
     sizeEx = QUOTE(safeZoneH * 0.025);
+};
+
+class GVAR(RscButtonDefault): GVAR(RscButton) {
     colorBackground[] = PRGBA(GUI_BCG_RGB);
+    colorBackgroundActive[] = PRGBA(GUI_BCG_RGB);
+    colorBackgroundDisabled[] = PRGB(GUI_BCG_RGB,0.5);
 };
 
 class GVAR(RscText) : RscText {
@@ -30,6 +32,14 @@ class GVAR(RscTextTitle) : GVAR(RscText) {
     colorBackground[] = PRGBA(GUI_BCG_RGB);
 };
 
+class GVAR(RscTitleBar) : RscText {
+    y = QUOTE(safeZoneY + 0.1 * safeZoneH);
+    h = QUOTE(safeZoneH * 0.03);
+    w = QUOTE(safeZoneW * 0.4);
+    sizeEx = QUOTE(safeZoneH * 0.025);
+    colorBackground[] = PRGBA(GUI_BCG_RGB);
+};
+
 class GVAR(Dialog) {
     idd = IDD_GARAGE_DIALOG;
     movingEnable = 0;
@@ -37,16 +47,18 @@ class GVAR(Dialog) {
 
     class Controls {
         class Title: GVAR(RscTitleBar) {
-            text = CSTRING(GarageDialog_TitleMain);
+            text = CSTRING(GarageDialog_TitleMain_Label);
             x = QUOTE(safeZoneX + safeZoneW * 0.1);
         };
+
         class Branding: GVAR(RscTitleBar) {
-            text = CSTRING(GarageDialog_TitlePoweredBy);
+            text = CSTRING(GarageDialog_TitlePoweredBy_Label);
             x = QUOTE(safeZoneX + safeZoneW * 0.5);
             style = QUOTE(ST_RIGHT);
         };
 
         class Main: RscControlsGroup {
+            idc = IDC_RSCGARAGEDIALOG_GROUP_MAIN;
             x = QUOTE(safeZoneX + safeZoneW * 0.1);
             y = QUOTE(safeZoneY + safeZoneH * 0.13 + pixelH * 8);
             w = QUOTE(safeZoneW * 0.8);
@@ -69,7 +81,7 @@ class GVAR(Dialog) {
 
                     class Controls {
                         class PanelTitle: GVAR(RscTextTitle) {
-                            text = CSTRING(GarageDialog_PanelLeftTitle);
+                            text = CSTRING(GarageDialog_PanelLeftTitle_Label);
                             x = 0;
                             y = 0;
                             w = QUOTE(safeZoneW * 0.2 - pixelW * 16);
@@ -93,21 +105,21 @@ class GVAR(Dialog) {
                             class Controls {
                                 class SpawnButton: RscButton {
                                     idc = IDC_RSCGARAGEDIALOG_BTN_SPAWN;
-                                    text = CSTRING(GarageDialog_BtnSpawn);
+                                    text = CSTRING(GarageDialog_BtnSpawn_Label);
                                     x = 0;
                                     y = QUOTE(pixelH * 8);
                                     w = QUOTE((safeZoneW * 0.2 - pixelW * 16) / 2 - pixelW * 4);
                                     h = QUOTE(lineHeight);
                                 };
 
-                                class CloseButton: RscButton {
-                                    idc = IDC_RSCGARAGEDIALOG_BTN_CLOSE;
-                                    text = CSTRING(GarageDialog_BtnClose);
+                                class LockButton: RscButton {
+                                    idc = IDC_RSCGARAGEDIALOG_BTN_LOCK;
+                                    text = CSTRING(GarageDialog_BtnLock_Label);
+                                    tooltip = CSTRING(GarageDialog_BtnLock_Tooltip);
                                     x = QUOTE((safeZoneW * 0.2 - pixelW * 16) / 2 + pixelW * 4);
                                     y = QUOTE(pixelH * 8);
                                     w = QUOTE((safeZoneW * 0.2 - pixelW * 16) / 2 - pixelW * 4);
                                     h = QUOTE(lineHeight);
-                                    action = "closeDialog 0";
                                 };
                             };
                         };
@@ -119,10 +131,20 @@ class GVAR(Dialog) {
         class StatusBar: GVAR(RscTextTitle) {
             idc = IDC_RSCGARAGEDIALOG_TEXT_STATUSBAR;
             text = "";
-            x = QUOTE(safeZoneX + 0.1 * safeZoneW);
+            x = QUOTE(safeZoneX + safeZoneW * 0.1);
             y = QUOTE(safeZoneY + safeZoneH * 0.88 + pixelH * 16);
-            w = QUOTE(safeZoneW * 0.8);
+            w = QUOTE(safeZoneW * 0.725);
             h = QUOTE(lineHeight);
+        };
+
+        class BtnClose: GVAR(RscButtonDefault) {
+            idc = IDC_RSCGARAGEDIALOG_BTN_CLOSE;
+            text = CSTRING(GarageDialog_BtnClose_Label);
+            x = QUOTE(safeZoneX + safeZoneW * 0.825 + pixelW * 8);
+            y = QUOTE(safeZoneY + safeZoneH * 0.88 + pixelH * 16);
+            w = QUOTE(safeZoneW * 0.075 - pixelW * 8);
+            h = QUOTE(lineHeight);
+            action = "closeDialog 0";
         };
     };
 };
