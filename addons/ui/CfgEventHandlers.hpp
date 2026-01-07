fff5cb0e7d4cdab84bca76022c1353bf6631a1a4
Oliver Schieche <post@oliver-schieche.de>
2026-01-07 09:37:48 +0100

    Mutex stuff is better placed in core addon

--- addons/ui/CfgEventHandlers.hpp
+++ addons/ui/CfgEventHandlers.hpp
@@ -2,6 +2,5 @@ class Extended_PreInit_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
         clientInit = QUOTE(call COMPILE_SCRIPT(XEH_preInitClient));
-        serverInit = QUOTE(call COMPILE_SCRIPT(XEH_preInitServer));
     };
 };
