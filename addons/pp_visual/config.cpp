eafa8b3e02be0cd27156bf5b409bf5e8aaae0ea5
Oliver Schieche <post@oliver-schieche.de>
2026-01-09 10:40:21 +0100

    Visual position provider addon

---
+++ addons/pp_visual/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = CSTRING(component);
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {QUOTE(A3OVG_ADDON_PP)};
+        author = "$STR_A3OVG_Author";
+        authors[] = {};
+        url = "$STR_A3OVG_URL";
+        VERSION_CONFIG;
+    };
+};
+
+#include "config.hpp"
+#include "CfgEventHandlers.hpp"
